dabcbb72a37aeb139053e4d47359fcb612acf83e
Timothy Lundy <timlundy74@gmail.com>
2020-10-11 20:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
 }
+  // Feel free to move things around!
+ 
 
 var funkyFunction = function() {
   return function() {
